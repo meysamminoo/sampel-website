dbf9b318e1cf68cdff60ce25ea5e82e56c695106
meysam <meisam.design.ph@gmail.com>
2021-11-25 17:51:41 +0330

    add popup and overlay

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,1 +1,25 @@
+// ! Selectors
+const popup = document.querySelector('.popup');
+const loginButton = document.querySelector('.global-login');
+const closePopup = document.querySelector('.fa-times');
+const overlay = document.querySelector('.overlay');
 
+// ! Add Event Listeners
+loginButton.addEventListener('click', showModal);
+closePopup.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// ! Functions
+// todo: show popup on click login button
+function showModal(){
+  popup.classList.add('active');
+  document.body.style.overflow = 'hidden';
+  overlay.classList.add('active');
+}
+
+// todo: close popup on click close item in header popup
+function closeModal(){
+  popup.classList.remove('active');
+  document.body.style.overflow = 'visible';
+  overlay.classList.remove('active');
+}
